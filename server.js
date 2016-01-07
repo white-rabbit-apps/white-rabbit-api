69ce1b1edadf7d8544dfa3a69c8d5bc224143191
Michael Bina <michaelbina@BinaBook-Pro.local>
2016-01-07 04:02:21 -0800

    committed by parse-cli at: 2016-01-07 04:02:21.468101654 -0800 PST

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(require("./cloud/prerenderio.js").setAdaptor(parseAdaptor(Parse)).set("p
 app.set("view engine", "jade");
 
 app.use(rewrite(/^(.*)$/, '/#/$1'));
+app.use(rewrite('/js/*', '/public/assets/js/$1'));
 
   // app.get('/*', function (req, res) {
   //     res.render('./index');
