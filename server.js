29e0dd132ebc116cbe6b031359033ff43a02e640
Michael Bina <michaelbina@BinaBook-Pro.local>
2016-01-07 10:39:36 -0800

    committed by parse-cli at: 2016-01-07 10:39:36.007195995 -0800 PST

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ var http = require('http'),
     bodyParser = require('body-parser'),
     Parse = require('parse/node'),
     ParseCloud = require('parse-cloud-express'),
-    parseAdaptor = require("./cloud/prerender-parse.js"),
-    rewriter = require('express-rewrite');
+    parseAdaptor = require('./cloud/prerender-parse.js');
 
 
 
 var app = express();
-
-// app.use(rewriter);
-//
-// app.get('/cat/:name', rewriter.rewrite('/#/cat/$1'));
 
 // Import your cloud code (which configures the routes)
 require('./cloud/main.js');
@@ -23,38 +18,18 @@ app.use(require("./cloud/prerenderio.js").setAdaptor(parseAdaptor(Parse)).set("p
 
 app.set("view engine", "jade");
 
-// app.get(/(?:^|[^#])\/cat\/(.*)$/, rewrite('\/#\/$2'));
-// app.get(/^\/cats\/(.*)$/, rewrite('/#/cat/$1'));
-// app.use(rewrite('/js/*', '/public/assets/js/$1'));
-
-  // app.get('/*', function (req, res) {
-  //     res.render('./index');
-  // });
-
 // Host static files from public/
 app.use(express.static(__dirname + '/public'));
-
 
 app.get('/*', function(request, response, next) {
   response.sendfile(__dirname + '/public/index.html');
 });
-// app.get('/location/*', function(request, response, next) {
-//   response.sendfile(__dirname + '/public/index.html');
-// });
-// app.get('/shelter/*', function(request, response, next) {
-//   response.sendfile(__dirname + '/public/index.html');
-// });
-// app.get('/hashtag/*', function(request, response, next) {
-//   response.sendfile(__dirname + '/public/index.html');
-// });
-
-
-
 
 // Catch all unknown routes.
 app.all('/', function(request, response) {
   response.status(404).send('Page not found.');
 });
+
 /*
  * Launch the HTTP server
  */
