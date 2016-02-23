d5cd9814bfb1c07c34c7fd3733fd5d0c69321790
Michael Bina <michaelbina@icloud.com>
2016-02-22 21:20:20 -0800

    committed by parse-cli at: 2016-02-22 21:20:19.960012814 -0800 PST

--- server.js
+++ server.js
@@ -63,9 +63,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(connect_s4a("d3c44980d364f87184334d863759dbe7"));
 // app.use(prerender);
 
-app.get('/*', function(request, response, next) {
-  response.sendFile(__dirname + '/public/index.html');
-});
+// app.get('/*', function(request, response, next) {
+//   response.sendFile(__dirname + '/public/index.html');
+// });
 
 var mountPath = process.env.PARSE_MOUNT || '/api';
 app.use(mountPath, api);
