deb982831c717e4c7c6292f00514918840ecf23c
Michael Bina <michaelbina@icloud.com>
2016-05-27 14:26:50 -0700

    committed by parse-cli at: 2016-05-27 14:26:50.03826266 -0700 PDT

--- public/js/app.js
+++ public/js/app.js
@@ -974,7 +974,7 @@ app.factory('Location', function(Parse) {
       return Location.__super__.constructor.apply(this, arguments);
     }
 
-    Location.configure("Location", "type", "name", "short_name", "email", "address", "city", "state", "zip", "geo", "geoForCity", "phone", "website", "facebookPageId", "twitterId", "instagramId", "instagramPlaceId", "youtubeUrl", "pinterestId", "yelpBusinessId", "logo", "types", "animals", "notes");
+    Location.configure("Location", "type", "name", "short_name", "email", "address", "city", "state", "zip", "geo", "geoForCity", "phone", "website", "facebookPageId", "twitterId", "instagramId", "instagramPlaceId", "youtubeUrl", "pinterestId", "yelpBusinessId", "logo", "types", "animals", "notes", "websiteDown");
 
     return Location;
 
